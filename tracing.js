0c83e73a423ac14d18c58e103a60624c6082cca5
anna <chenanna@ufl.edu>
2025-04-24 23:58:50 -0400

    OTLP Exporter changes

--- tracing.js
+++ tracing.js
@@ -2,31 +2,41 @@ const { resourceFromAttributes } = require('@opentelemetry/resources');
 const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const {
- getNodeAutoInstrumentations,
+  getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+
+// added
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+
 const {
- ConsoleMetricExporter,
- PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+  PeriodicExportingMetricReader,
 } = require('@opentelemetry/sdk-metrics');
-//Instrumentations
+
+// Instrumentations
 const { ExpressInstrumentation } =
- require("opentelemetry-instrumentation-express");
+  require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } =
- require("@opentelemetry/instrumentation-mongodb");
+  require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+
 const sdk = new NodeSDK({
- resource: resourceFromAttributes({
- [ATTR_SERVICE_NAME]: "todo-service"
- }),
- traceExporter: new ConsoleSpanExporter(),
- metricReader: new PeriodicExportingMetricReader({
- exporter: new ConsoleMetricExporter(),
- }),
- instrumentations: [
- getNodeAutoInstrumentations(),
- new ExpressInstrumentation(),
- new MongoDBInstrumentation(),
- new HttpInstrumentation()],
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "todo-service"
+  }),
+  // updated
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces'
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new ExpressInstrumentation(),
+    new MongoDBInstrumentation(),
+    new HttpInstrumentation()
+  ],
 });
+
 sdk.start();
